bc976a76d605568e31b12533e39b4aa3de58ba75
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-06-28 14:55:20 +0530

    Find n\kth node of linked list

---
+++ Find n\kth node in linked list.cpp
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    // your code here
+    if(head==NULL)return -1;
+    int N=0;
+    Node* temp=head;
+    while(temp!=NULL){
+        N++;
+        temp=temp->next;
+    }
+    
+    int n=ceil(float(N)/float(k));
+    int i=1;
+    temp=head;
+    while(i<n){
+        temp=temp->next;
+        i++;
+    }
+    return temp->data;
+}
